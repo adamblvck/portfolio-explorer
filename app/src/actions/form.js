0ab57c7b266e0810df3c26534d79dd10918bd5ba
Adam Blvck <eragon.blizzard@gmail.com>
2019-04-17 19:05:45 +0200

    Adding up to redux the concept form

--- app/src/actions/form.js
+++ app/src/actions/form.js
@@ -1,6 +1,9 @@
 export const OPEN_GROUP_FORM = "open_group_form";
 export const CLOSE_GROUP_FORM = "close_group_form";
 
+export const OPEN_CONCEPT_FORM = "open_concept_form";
+export const CLOSE_CONCEPT_FORM = "close_concept_form";
+
 export function openGroupForm(params) {
     const payload = {
         mode: params.mode,
@@ -15,9 +18,30 @@ export function openGroupForm(params) {
     }
 }
 
-export function closeGroupForm(params) {
+export function closeGroupForm() {
     return {
         type: CLOSE_GROUP_FORM,
+        payload: {}
+    }
+}
+
+export function openConceptForm(params) {
+    const payload = {
+        mode: params.mode,
+        open: true,
+        form_type: "concept",
+        initialValues: params.initialValues
+    };
+
+    return {
+        type: OPEN_GROUP_FORM,
+        payload: payload
+    }
+}
+
+export function closeConceptForm() {
+    return {
+        type: CLOSE_CONCEPT_FORM,
         payload: {}
     }
 }
--- app/src/reducers/reducer_forms.js
+++ app/src/reducers/reducer_forms.js
@@ -1,19 +1,30 @@
-import { OPEN_GROUP_FORM, CLOSE_GROUP_FORM } from '../actions/form';
+import { OPEN_GROUP_FORM, CLOSE_GROUP_FORM, OPEN_CONCEPT_FORM, CLOSE_CONCEPT_FORM } from '../actions/form';
 
 export default function(state = null, action) {
     switch(action.type){
         case OPEN_GROUP_FORM:
-            const new_state = { ...action.payload};
-            return new_state;
+            const new_group_state = { ...action.payload};
+            return new_group_state;
             
         case CLOSE_GROUP_FORM:
             // keep previous position
-            const close_state = {
+            const close_group_state = {
                 ...state,
                 open: false
             }
+            return close_group_state;
 
-            return close_state;
+        case OPEN_CONCEPT_FORM:
+            const new_concept_state = { ...action.payload};
+            return new_concept_state;
+
+        case CLOSE_CONCEPT_FORM:
+            // keep previous position
+            const close_concept_state = {
+                ...state,
+                open: false
+            }
+            return close_concept_state;
 
         default:
             return state;
