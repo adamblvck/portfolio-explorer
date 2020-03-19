d8d0480d6838c7421bfa8f908588839686e2d110
adam_blvck <contact@adamblvck.com>
2020-03-19 20:18:56 +0100

    spelling fix

--- app/src/containers/forms/gradient_helper.js
+++ app/src/containers/forms/gradient_helper.js
@@ -9,11 +9,11 @@ const gradients = [
 	{ value: 'linear-gradient(45deg, #4532E6, #1cb5e0)', name: 'Blue Wave', textColor: white, headerColor: '#4532E6'},
 	{ value: 'linear-gradient(45deg, #2bc0e4, #eaecc6)', name: 'Shallow Beach', textColor: dark, headerColor: '#2bc0e4'},
 
-	{ value: 'linear-gradient(45deg, #FC466B 0%, #3F5EFB 100%)', name: 'Red To Voilet', textColor: white, headerColor: '#FC466B'},
+	{ value: 'linear-gradient(45deg, #FC466B 0%, #3F5EFB 100%)', name: 'Red To Violet', textColor: white, headerColor: '#FC466B'},
 	{ value: 'linear-gradient(45deg, #1CB5E0 0%, #000851 100%)', name: 'Living Ocean', textColor: white, headerColor: '#1CB5E0'},
 	{ value: 'linear-gradient(45deg, #3F2B96 0%, #A8C0FF 100%)', name: 'Trusty Approach', textColor: white, headerColor: '#3F2B96'},
 	{ value: 'linear-gradient(45deg, #4b6cb7 0%, #182848 100%)', name: 'Black Sea Residu', textColor: white, headerColor: '#4b6cb7'},
-	{ value: 'linear-gradient(45deg, #0700b8 0%, #00ff88 100%)', name: 'Overused Bolt', textColor: white, headerColor: '#0700b8'},
+	{ value: 'linear-gradient(45deg, #0700b8 0%, #00ff88 100%)', name: 'Overused Bold', textColor: white, headerColor: '#0700b8'},
 	{ value: 'linear-gradient(45deg, #fcff9e 0%, #c67700 100%)', name: 'Sunlit Cinamon', textColor: dark, headerColor: '#c67700'},
 	{ value: 'linear-gradient(45deg, #9ebd13 0%, #008552 100%)', name: 'Lemon With Avocado', textColor: white, headerColor: '#9ebd13'},
 	{ value: 'linear-gradient(45deg, #FDBB2D 0%, #22C1C3 100%)', name: 'Tropical Parrot', textColor: dark, headerColor: '#FDBB2D'},
